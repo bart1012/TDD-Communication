db3b45308c04ba9b255e33bfe4f32300ee7e232b
bart <bielski.bart1012@gmail.com>
2024-11-19 10:36:56 +0000

    Create NumberSum class and its associated tests

--- TDDCommunicationChallenge/TDD.Tests/UnitTest1.cs
+++ TDDCommunicationChallenge/TDD.Tests/UnitTest1.cs
@@ -1,17 +1,35 @@
+using TDDCommunicationChallenge;
+
 namespace TDD.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        NumberSum numberSum;
+
+        private static readonly object[] TestCasesForNumberSum =
         {
+            new object[]{new int[] { 5, 2, 6, 10, 22 }, 21},
+            new object[]{new int[] { 1, 2, 3, 4, 5 }, 9},
+            new object[]{new int[] { 1 }, 0},
+            new object[]{new int[] { 1,2 }, 0},
+            new object[]{new int[] { 1,2,3 }, 2},
 
-        }
 
+        };
+
+        [TestCaseSource(nameof(TestCasesForNumberSum))]
         [Test]
-        public void Test1()
+        public void NumberSum_returns_intended_number(int[] input, int expectedResult)
         {
-            Assert.Pass();
+            //Arrange
+            numberSum = new();
+
+            //Act
+            var methodCallResult = numberSum.SumMiddleNumbers(input);
+
+            //Assert
+            Assert.That(methodCallResult == expectedResult);
+
         }
     }
 }
---
+++ TDDCommunicationChallenge/TDDCommunicationChallenge/NumberSum.cs
@@ -0,0 +1,19 @@
+﻿namespace TDDCommunicationChallenge
+{
+    public class NumberSum
+    {
+
+        public int SumMiddleNumbers(int[] numbers)
+        {
+            if (numbers.Length <= 1)
+            {
+                return 0;
+            }
+            int sumOfAllNumbers = numbers.Sum();
+            int resultNumber = sumOfAllNumbers;
+            resultNumber -= (numbers.Max() + numbers.Min());
+            return resultNumber;
+        }
+
+    }
+}
